4d5b65a03cb9be31b34ef005f915ac17e2cea4af
Shifa Zahra <140159309+ShifaZahra123@users.noreply.github.com>
2024-08-29 17:53:31 +0500

    app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def vector_embedding():  ## Read pdf file, Convert into chunks, apply Google Gen
     if "vectors" not in st.session_state:
 
         st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-001") ## Google model for embedding
-        st.session_state.loader=PyPDFDirectoryLoader("./us_census") ## Data Ingestion
+        st.session_state.loader=PyPDFDirectoryLoader("./Latest_Research_Paper") ## Data Ingestion
         st.session_state.docs=st.session_state.loader.load() ## Document Loading
         st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation, chunk_overlap is used so that nothing will be missed
         st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
